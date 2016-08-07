7a1d676ee2569ae621c4bfab43507cb04267bac9
ziphrax <daverival@hotmail.com>
2016-08-07 01:39:48 +0100

    added the example code and functions

---
+++ fb_messenger.js
@@ -0,0 +1,27 @@
+var config = require('./config');
+
+function verifyRequestSignature(req, res, buf) {
+  var signature = req.headers["x-hub-signature"];
+
+  if (!signature) {
+    // For testing, let's log an error. In production, you should throw an
+    // error.
+    console.error("Couldn't validate the signature.");
+  } else {
+    var elements = signature.split('=');
+    var method = elements[0];
+    var signatureHash = elements[1];
+
+    var expectedHash = crypto.createHmac('sha1', config.APP_SECRET)
+                        .update(buf)
+                        .digest('hex');
+
+    if (signatureHash != expectedHash) {
+      throw new Error("Couldn't validate the request signature.");
+    }
+  }
+}
+
+module.exports = {
+  verifyRequestSignature : verifyRequestSignature
+}
