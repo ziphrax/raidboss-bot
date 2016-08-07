0774ea6039bc7f59ce8a082b0f7f0b4e460468a4
ziphrax <daverival@hotmail.com>
2016-08-07 01:20:35 +0100

    added validation function for fb webhooks

--- server.js
+++ server.js
@@ -1,8 +1,22 @@
 var express = require('express'),
     app = express(),
-    port = 3000;
+    port = 3000,
+    config = require('./config');
+
 
 app.use(express.static('client'));
+
+app.get('/webhook', function(req, res) {
+  if (req.query['hub.mode'] === 'subscribe' &&
+      req.query['hub.verify_token'] === config.VALIDATION_TOKEN) {
+    console.log("Validating webhook");
+    res.status(200).send(req.query['hub.challenge']);
+  } else {
+    console.error("Failed validation. Make sure the validation tokens match.");
+    res.sendStatus(403);
+  }
+});
+
 var server = app.listen(port,function(err){
   if(err){
     console.log(err);
